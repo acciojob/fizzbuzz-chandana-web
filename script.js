6b7bf179993ab18ebdaae3d50b72ba274bdc9e9d
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-22 16:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function num() {
+    arr='';
+    for(let i=1;i<=20;i++){
+        if(i%3 === 0 && i%5 === 0){
+            arr += 'FizzBuzz\n';
+        }else if(i%3===0){
+            arr +='Fizz\n';
+        }else if(i%5===0){
+            arr+='Buzz\n';
+        }else{
+            arr+=i+'\n';
+        }
+    }
+    alert(arr);
+}
+num();
